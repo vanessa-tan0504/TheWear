ebf453815c6dd1cc654ae83c5acfd85973cd1871
vanessa-tan0504 <p17009280@student.newinti.edu.my>
2020-09-09 00:09:45 +0800

    intro pager done

--- app/src/main/java/com/thewear/thewearapp/MainActivity.java
+++ app/src/main/java/com/thewear/thewearapp/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 number++;
-                Intent swap = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent swap = new Intent(MainActivity.this, IntroActivity.class);
                 //swap.putExtra("NUMBER",number);
                 startActivity(swap);
             }
@@ -219,5 +219,20 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+    }
+
+    //hide status bar and below softkey
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
