2583bcaf13e1ed374bfea34dc82309eb588f98a2
vanessa-tan0504 <p17009280@student.newinti.edu.my>
2020-09-23 23:50:27 +0800

    idv item revision1

--- app/src/main/java/com/thewear/thewearapp/IdvItemActivity.java
+++ app/src/main/java/com/thewear/thewearapp/IdvItemActivity.java
@@ -15,25 +15,31 @@ import android.view.ViewOutlineProvider;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.collection.LLRBNode;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
 
 import java.util.List;
 
+import static com.google.firebase.auth.FirebaseAuth.getInstance;
+
 public class IdvItemActivity extends AppCompatActivity {
 
 
-    FirebaseFirestore db;
+    FirebaseFirestore db,db_price;
     ImageView back;
     float rad=300f; //radiius
     private DotsIndicator dotsIndicator;
@@ -43,6 +49,8 @@ public class IdvItemActivity extends AppCompatActivity {
     private TextView qty;
     private int amt = 1;
     private ScrollView scrollView;
+    private RadioGroup radioGroupSize,radioGroupCol;
+    String userID;
 
 
     @Override
@@ -57,18 +65,21 @@ public class IdvItemActivity extends AppCompatActivity {
         dotsIndicator=findViewById(R.id.dots_indicator);
         tvtitle=findViewById(R.id.item_title);
         tvdesc = findViewById(R.id.item_desc);
-        rad_s=findViewById(R.id.rad_s);
-        rad_m=findViewById(R.id.rad_m);
-        rad_l=findViewById(R.id.rad_l);
-        rad_x=findViewById(R.id.rad_x);
-        rad_black=findViewById(R.id.rad_black);
-        rad_blue=findViewById(R.id.rad_blue);
-        rad_white=findViewById(R.id.rad_white);
+        //rad_s=findViewById(R.id.rad_s);
+//        rad_m=findViewById(R.id.rad_m);
+//        rad_l=findViewById(R.id.rad_l);
+//        rad_x=findViewById(R.id.rad_x);
+//        rad_black=findViewById(R.id.rad_black);
+//        rad_blue=findViewById(R.id.rad_blue);
+//        rad_white=findViewById(R.id.rad_white);
         btnminus = findViewById(R.id.qyt_minus);
         btnplus = findViewById(R.id.qyt_add);
         qty = findViewById(R.id.qyt_text);
         scrollView = findViewById(R.id.idvact);
         btncart = findViewById(R.id.add_cart);
+        radioGroupSize = findViewById(R.id.radioGroupsize);
+        radioGroupCol = findViewById(R.id.radioGroupcol);
+        userID= FirebaseAuth.getInstance().getCurrentUser().getUid();
 
 
 
@@ -108,14 +119,109 @@ public class IdvItemActivity extends AppCompatActivity {
                         IdvItemImageAdapter adapter = new IdvItemImageAdapter(getApplicationContext(),url);
                         viewPager.setAdapter(adapter);
                         dotsIndicator.setViewPager(viewPager);
-
                         //title and desc
                         String title = document.getString("title");
                         String desc = document.getString("desc");
-                        double price = document.getDouble ("price");
+                        final double price = document.getDouble("price");
                         tvtitle.setText(title);
                         tvdesc.setText(desc);
-                        btncart.setText(String.format("ADD TO CART\t    RM%.2f",price));
+                        btncart.setText(String.format("ADD TO CART\t\tRM %.2f",price*amt));
+
+                        // radio listener-------------------------------------------------------------------------------
+                        radioGroupSize.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                                switch (checkedId){
+                                    case R.id.rad_s:
+                                        Toast.makeText(IdvItemActivity.this, "s", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case R.id.rad_m:
+                                        Toast.makeText(IdvItemActivity.this, "m", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case R.id.rad_l:
+                                        Toast.makeText(IdvItemActivity.this, "l", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case R.id.rad_x:
+                                        Toast.makeText(IdvItemActivity.this, "x", Toast.LENGTH_SHORT).show();
+                                        break;
+                                }
+                            }
+                        });
+
+                        radioGroupCol.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                                switch (checkedId){
+                                    case R.id.rad_black:
+                                        Toast.makeText(IdvItemActivity.this, "blcc", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case R.id.rad_blue:
+                                        Toast.makeText(IdvItemActivity.this, "blue", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case R.id.rad_white:
+                                        Toast.makeText(IdvItemActivity.this, "lwhite", Toast.LENGTH_SHORT).show();
+                                        break;
+
+                                }
+                            }
+                        });
+                        // radio listener end----------------------------------------------------------------------------
+
+                        //quantity button--------------------------------------------------
+                        //button gesture pressing listener
+                        btnplus.setOnTouchListener(new View.OnTouchListener() {
+                            @Override
+                            public boolean onTouch(View v, MotionEvent event) {
+                                if(event.getAction()== MotionEvent.ACTION_DOWN){
+                                    //when button pressed
+                                    btnplus.setBackgroundResource(R.drawable.rounded_btn_black);
+                                    btnplus.setTextColor(Color.parseColor("#FFFFFF"));
+                                    amt++;
+                                    qty.setText(String.valueOf(amt));
+                                }
+                                if(event.getAction()==MotionEvent.ACTION_UP){
+                                    //when button released
+                                    btnplus.setBackgroundResource(R.drawable.rounded_btn_whitestroke);
+                                    btnplus.setTextColor(Color.parseColor("#000000"));
+                                }
+                                btncart.setText(String.format("ADD TO CART\t\tRM %.2f",price*amt));
+                                return false;
+
+                            }
+                        });
+
+                        btnminus.setOnTouchListener(new View.OnTouchListener() {
+                            @Override
+                            public boolean onTouch(View v, MotionEvent event) {
+                                if(event.getAction()== MotionEvent.ACTION_DOWN){
+                                    //when button pressed
+                                    if(amt==1){
+                                        Snackbar.make(scrollView,"Quantity cannot be less than 1",Snackbar.LENGTH_LONG).show();
+                                    }
+                                    else if(amt>1){
+                                        btnminus.setEnabled(true);
+                                        btnminus.setBackgroundResource(R.drawable.rounded_btn_black);
+                                        btnminus.setTextColor(Color.parseColor("#FFFFFF"));
+                                        amt--;
+                                        qty.setText(String.valueOf(amt));
+                                    }
+                                }
+                                if(event.getAction()==MotionEvent.ACTION_UP){
+                                    //when button released
+                                    btnminus.setBackgroundResource(R.drawable.rounded_btn_whitestroke);
+                                    btnminus.setTextColor(Color.parseColor("#000000"));
+                                }
+                                btncart.setText(String.format("ADD TO CART\t\tRM %.2f",price*amt));
+                                return false;
+                            }
+                        });
+
+                        btncart.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(IdvItemActivity.this, userID+"", Toast.LENGTH_SHORT).show();
+                            }
+                        });
 
                     }
                 });
@@ -126,70 +232,10 @@ public class IdvItemActivity extends AppCompatActivity {
 
         //item viewpager end--------------------------------------------------------------
 
-        //size radio listener-------------------------------------------------------------------------------
 
-        rad_s.setOwnOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Toast.makeText(IdvItemActivity.this, "clicked"+isChecked, Toast.LENGTH_SHORT).show();
-            }
-        });
-        rad_m.setOwnOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            }
-        });
-        //size radio listener end----------------------------------------------------------------------------
 
-        //quantity button--------------------------------------------------
-        //button gesture pressing listener
-        btnplus.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction()== MotionEvent.ACTION_DOWN){
-                    //when button pressed
-                    btnplus.setBackgroundResource(R.drawable.rounded_btn_black);
-                    btnplus.setTextColor(Color.parseColor("#FFFFFF"));
-                    amt++;
-                    qty.setText(String.valueOf(amt));
-                }
-                if(event.getAction()==MotionEvent.ACTION_UP){
-                    //when button released
-                    btnplus.setBackgroundResource(R.drawable.rounded_btn_whitestroke);
-                    btnplus.setTextColor(Color.parseColor("#000000"));
-                }
-                Toast.makeText(IdvItemActivity.this, amt+"", Toast.LENGTH_SHORT).show();
-                return false;
 
-            }
-        });
 
-        btnminus.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction()== MotionEvent.ACTION_DOWN){
-                    //when button pressed
-                    if(amt==1){
-                        Snackbar.make(scrollView,"Quantity cannot be less than 1",Snackbar.LENGTH_LONG).show();
-                    }
-                    else if(amt>1){
-                        btnminus.setEnabled(true);
-                        btnminus.setBackgroundResource(R.drawable.rounded_btn_black);
-                        btnminus.setTextColor(Color.parseColor("#FFFFFF"));
-                        amt--;
-                        qty.setText(String.valueOf(amt));
-                    }
-                }
-                if(event.getAction()==MotionEvent.ACTION_UP){
-                    //when button released
-                    btnminus.setBackgroundResource(R.drawable.rounded_btn_whitestroke);
-                    btnminus.setTextColor(Color.parseColor("#000000"));
-                }
-                Toast.makeText(IdvItemActivity.this, amt+"", Toast.LENGTH_SHORT).show();
-
-                return false;
-            }
-        });
 
 
     }
