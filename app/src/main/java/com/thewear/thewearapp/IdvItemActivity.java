83d1543a4dc46ff24c11299f10b68efb16b3d897
vanessa-tan0504 <p17009280@student.newinti.edu.my>
2020-09-23 14:53:06 +0800

    item ui done

--- app/src/main/java/com/thewear/thewearapp/IdvItemActivity.java
+++ app/src/main/java/com/thewear/thewearapp/IdvItemActivity.java
@@ -5,18 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Outline;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewOutlineProvider;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.collection.LLRBNode;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,6 +39,10 @@ public class IdvItemActivity extends AppCompatActivity {
     private DotsIndicator dotsIndicator;
     TextView tvtitle, tvdesc;
     private CustomRadioBtn rad_s,rad_m,rad_l,rad_x,rad_white,rad_blue,rad_black;
+    private Button btnplus, btnminus, btncart;
+    private TextView qty;
+    private int amt = 1;
+    private ScrollView scrollView;
 
 
     @Override
@@ -54,6 +64,12 @@ public class IdvItemActivity extends AppCompatActivity {
         rad_black=findViewById(R.id.rad_black);
         rad_blue=findViewById(R.id.rad_blue);
         rad_white=findViewById(R.id.rad_white);
+        btnminus = findViewById(R.id.qyt_minus);
+        btnplus = findViewById(R.id.qyt_add);
+        qty = findViewById(R.id.qyt_text);
+        scrollView = findViewById(R.id.idvact);
+        btncart = findViewById(R.id.add_cart);
+
 
 
 
@@ -96,8 +112,10 @@ public class IdvItemActivity extends AppCompatActivity {
                         //title and desc
                         String title = document.getString("title");
                         String desc = document.getString("desc");
+                        double price = document.getDouble ("price");
                         tvtitle.setText(title);
                         tvdesc.setText(desc);
+                        btncart.setText(String.format("ADD TO CART\t    RM%.2f",price));
 
                     }
                 });
@@ -121,7 +139,63 @@ public class IdvItemActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             }
         });
+        //size radio listener end----------------------------------------------------------------------------
+
+        //quantity button--------------------------------------------------
+        //button gesture pressing listener
+        btnplus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()== MotionEvent.ACTION_DOWN){
+                    //when button pressed
+                    btnplus.setBackgroundResource(R.drawable.rounded_btn_black);
+                    btnplus.setTextColor(Color.parseColor("#FFFFFF"));
+                    amt++;
+                    qty.setText(String.valueOf(amt));
+                }
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    //when button released
+                    btnplus.setBackgroundResource(R.drawable.rounded_btn_whitestroke);
+                    btnplus.setTextColor(Color.parseColor("#000000"));
+                }
+                Toast.makeText(IdvItemActivity.this, amt+"", Toast.LENGTH_SHORT).show();
+                return false;
+
+            }
+        });
+
+        btnminus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()== MotionEvent.ACTION_DOWN){
+                    //when button pressed
+                    if(amt==1){
+                        Snackbar.make(scrollView,"Quantity cannot be less than 1",Snackbar.LENGTH_LONG).show();
+                    }
+                    else if(amt>1){
+                        btnminus.setEnabled(true);
+                        btnminus.setBackgroundResource(R.drawable.rounded_btn_black);
+                        btnminus.setTextColor(Color.parseColor("#FFFFFF"));
+                        amt--;
+                        qty.setText(String.valueOf(amt));
+                    }
+                }
+                if(event.getAction()==MotionEvent.ACTION_UP){
+                    //when button released
+                    btnminus.setBackgroundResource(R.drawable.rounded_btn_whitestroke);
+                    btnminus.setTextColor(Color.parseColor("#000000"));
+                }
+                Toast.makeText(IdvItemActivity.this, amt+"", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
+
+
     }
+
+
+
 
     //hide status bar and below softkey
     @Override
